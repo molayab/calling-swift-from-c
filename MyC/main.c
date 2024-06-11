d88248f543c5c51e15ea7e88beab3d20c4beb05e
Mateo Olaya <pub.mateo.olaya@gmail.com>
2024-06-10 23:14:07 -0500

    Initial Commit

---
+++ MyC/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  MyC
+//
+//  Created by Mateo Olaya on 10/06/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
